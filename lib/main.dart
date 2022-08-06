4797962af5c2250d6c0dabd987cc2c9d244632c1
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-08-06 14:25:00 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyExpenses',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expenses'),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('GRÁFICO'),
+              ),
+            ),
+            Card(
+              color: Colors.red,
+              child: Text('Transactions'),
+            )
+          ]),
+    );
+  }
+}
